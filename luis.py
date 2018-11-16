be7bb69af35bcb6c527433651b84a1b5bad1c3a7
catharsis96 <catharsis96@yandex.ru>
2018-11-16 23:01:48 +0300

    ledums2

--- luis.py
+++ luis.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # INSERT YOUR LUIS ENDPOINT URL HERE *it's example URL*
-API_ENDPOINT = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/ee2f2869-98d2-476a-9559-cc8389609427?subscription-key=8bf34f219a6140b0a4f9c632f44d7d0d&timezoneOffset=-360&q="
+API_ENDPOINT = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/4559e6f0-c1d2-4e3c-a1c4-630472097533?subscription-key=16b85f73be424d1b9337dba9dcb18fe5&timezoneOffset=-360&q="
 
 def get_command(command):
 	r = requests.get(API_ENDPOINT + command)
